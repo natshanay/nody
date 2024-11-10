3198cf3762b7c8ee363b0f27020796dca7df0380
Natnael Chala <natnaelchala10@gmail.com>
2024-11-10 09:21:38 -0800

    that is

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 2000;
 const dbconnection = require("./db/dbConfig");
 
 app.use(express.static('apple'));
